14fb345a98d3d894c236252ef9583f8705df1135
lachimolala-109 <95944246+lachimolala-109@users.noreply.github.com>
2024-06-06 11:59:07 -0400

    dfdsfs

--- FPT_Main/FPT_Main.ino
+++ FPT_Main/FPT_Main.ino
@@ -6,14 +6,16 @@
 #define GL2 8
 
 unsigned long previousMillis = 0;
-const long interval = 3000; // Interval in milliseconds
+const long interval = 1000; // Interval in milliseconds
 
 int state1 = 0; // 0: red, 1: yellow, 2: green
 int state2 = 2; // 0: red, 1: yellow, 2: green
 
-const unsigned long redDuration = 36000; // 36 seconds
-const unsigned long yellowDuration = 6000; // 6 seconds
-const unsigned long greenDuration = 20000; // 20 seconds
+const unsigned long redDuration = 30000; // 30 seconds
+const unsigned long yellowDuration = 5000; // 5 seconds
+const unsigned long greenDuration = 30000; // 30 seconds
+
+unsigned long currentMillis;
 
 void setup() {
   // Serial setup
@@ -29,28 +31,28 @@ void setup() {
 }
 
 void loop() {
-  // Blink test 1
-  digitalWrite(RL1, HIGH);
-  digitalWrite(RL2, HIGH);
-  delay(1000);
-  digitalWrite(RL1, LOW);
-  digitalWrite(RL2, LOW);
-  digitalWrite(YL1, HIGH);
-  digitalWrite(YL2, HIGH);
-  delay(1000);
-  digitalWrite(YL1, LOW);
-  digitalWrite(YL2, LOW);
-  digitalWrite(GL1, HIGH);
-  digitalWrite(GL2, HIGH);
-  delay(1000);
-  digitalWrite(GL1, LOW);
-  digitalWrite(GL2, LOW);
+  // // Blink test 1
+  // digitalWrite(RL1, HIGH);
+  // digitalWrite(RL2, HIGH);
+  // delay(1000);
+  // digitalWrite(RL1, LOW);
+  // digitalWrite(RL2, LOW);
+  // digitalWrite(YL1, HIGH);
+  // digitalWrite(YL2, HIGH);
+  // delay(1000);
+  // digitalWrite(YL1, LOW);
+  // digitalWrite(YL2, LOW);
+  // digitalWrite(GL1, HIGH);
+  // digitalWrite(GL2, HIGH);
+  // delay(1000);
+  // digitalWrite(GL1, LOW);
+  // digitalWrite(GL2, LOW);
 
-//   // Update the traffic lights
-//   unsigned long currentMillis = millis();
+ // Update the traffic lights
+  currentMillis = millis();
 
-//   if (currentMillis - previousMillis >= interval) {
-//     previousMillis = currentMillis;
-//     updateTrafficLights();
-// }
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    updateTrafficLights();
+  }
 }
